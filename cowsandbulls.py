88dfc9c65182bf8de1fdc02df3366f84fc994dc3
unknown <ngransky@gmail.com>
2019-01-07 15:59:47 -0500

    Add check function to calculate number of cows and bulls

--- cowsandbulls.py
+++ cowsandbulls.py
@@ -1,5 +1,15 @@
 from random import randint
 
+def check(num, guess):
+	cows = 0
+	bulls = 0
+	for i in range(len(num)):
+		if guess[i] == number[i]:
+			cows += 1
+		elif guess[i] in num:
+			bulls += 1
+	return [cows, bulls]
+
 number = ''
 for i in range(4):
 	number += str(randint(0, 9))
@@ -8,3 +18,6 @@ print(number)
 
 while True:
 	guess = input('Give me your best guess: ')
+	cowsandbulls = check(number, guess)
+	print("Cows: " + str(cowsandbulls[0]))
+	print("Bulls: " + str(cowsandbulls[1]))
